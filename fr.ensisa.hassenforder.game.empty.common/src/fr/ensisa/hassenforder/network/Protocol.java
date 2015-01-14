6d72cabe81130ad53b9a1ef3481d15ce6a620ced
DaftSpirit <joris.rat@gmail.com>
2015-01-14 15:31:30 +0100

    Defined DISCRIMINANTS

--- fr.ensisa.hassenforder.game.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
+++ fr.ensisa.hassenforder.game.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
@@ -2,7 +2,15 @@ package fr.ensisa.hassenforder.network;
 
 public interface Protocol {
 
-	static final public int GAME_PORT_ID = 0;
+	static final public int GAME_PORT_ID = 12322;
+	
+	static final public int STAT = 10;
+	static final public int PROD = 20;
+	static final public int SHOP = 30;
+	static final public int ADD = 40;
+	static final public int SUB = 50;
+	static final public int CLEAR = 60;
+	static final public int CONSUME = 70;
 
 	public static final Object EXIT_TEXT = "exit";
 
