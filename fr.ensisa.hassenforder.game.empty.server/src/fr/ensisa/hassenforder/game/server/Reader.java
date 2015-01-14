1835e37b9555c4cf392ced6a7033c36a5ce754a5
DaftSpirit <joris.rat@gmail.com>
2015-01-14 16:11:12 +0100

    Le reader lit en fonction des discriminants dans le protocol.java

--- fr.ensisa.hassenforder.game.empty.server/src/fr/ensisa/hassenforder/game/server/Reader.java
+++ fr.ensisa.hassenforder.game.empty.server/src/fr/ensisa/hassenforder/game/server/Reader.java
@@ -12,12 +12,88 @@ public class Reader extends BasicAbstractReader {
 		super (inputStream);
 	}
 
+	/**
+	 * @author joris
+	 * select which type of data we read
+	 * 0 is error
+	 */
 	public void receive() {
 		type = readInt ();
 		switch (type) {
 		case 0 :
 			break;
+		case 10 :
+			readStat();
+			break;
+		case 20 :
+			readProd();
+			break;
+		case 30 :
+			readShop();
+			break;
+		case 40 :
+			readAdd();
+			break;
+		case 50 : 
+			readSub();
+			break;
+		case 60 :
+			readClear();
+			break;
+		case 70 :
+			readConsume();
+			break;
 		}
+	}
+
+	private void readConsume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readClear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readSub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readAdd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readShop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readProd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readStat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readTab() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
